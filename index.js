9af3e4a3c5a631569ee60c2577072e1625c251cc
rodrigo <rodrigo.engenheirodesoftware@gmail.com>
2022-10-14 22:29:01 -0300

    Usando req.params

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require("express")
 const app = express()
 const port =3000
 
+
 app.get('/', (req,res)=>{
     var nome = "rodrigo"
     var profissao = "programador"
     res.render("index", {
         nome: nome,
         profissao : profissao
+    })
+})
+
+app.get('/:nome/:profissao', (req,res)=>{
+    var nome1 = req.params.nome;
+    var profissao1 = req.params.profissao
+    res.render("index", {
+        nome: nome1,
+        profissao : profissao1
     })
 })
 
