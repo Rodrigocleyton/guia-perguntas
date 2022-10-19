823b9771421d0132da0fca21c9de2539f3d9ec31
rodrigo <rodrigo.engenheirodesoftware@gmail.com>
2022-10-18 23:17:59 -0300

    Inicio do projeto guia de perguntas

--- index.js
+++ index.js
@@ -8,35 +8,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get('/', (req,res)=>{
-    var nome = "rodrigo"
-    var profissao = "programador"
+   
     res.render("index", {
-        nome: nome,
-        profissao : profissao
+      
     })
 })
 
-app.get('/:nome/:profissao', (req,res)=>{
-    var nome1 = req.params.nome;
-    var profissao1 = req.params.profissao
-    var exibirMsg = false
-
-    var produtos = [
-        {nome: "Chocolate", preco: 4.99},
-        {nome: "Suco de Uva", preco: 11.99},
-        {nome: "goiabada", preco: 7.99}
-        
-    ]
-
-
-    res.render("index", {
-        nome: nome1,
-        profissao : profissao1,
-        msg: exibirMsg,
-        produtos: produtos
-        
-    })
-})
 
 
 
