5ffad9276319ace50d36fefdf8c00b54a5210075
Rodrigocleyton <88049769+Rodrigocleyton@users.noreply.github.com>
2022-10-29 00:39:31 -0300

    Capturando dados do formulário

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const port =3000
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 //ejs como view engine
 app.set('view engine', 'ejs');
 //config para usar aequivos estáticos no express(css, imagens, etc)
@@ -19,7 +23,9 @@ app.get("/perguntar", (req, res)=>{
 })
 //recebe os dados do formulário 
 app.post("/salvarpergunta", (req, res)=>{
-    res.send("formulário recebido")
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send(titulo + " " + descricao)
 })
 
 
